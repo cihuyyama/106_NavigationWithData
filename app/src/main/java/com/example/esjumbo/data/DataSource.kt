1664674d23917bc571dbc3171b559c7a04c2c594
cihuyyama <miqbalalhabib@gmail.com>
2023-11-10 02:31:56 +0700

    first commit

---
+++ app/src/main/java/com/example/esjumbo/data/DataSource.kt
@@ -0,0 +1,13 @@
+package com.example.esjumbo.data
+
+import com.example.esjumbo.R
+
+object DataSource {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
